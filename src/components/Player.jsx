d3f32728e8a68cc02825cd1dd9c7298a2944abe1
Edinson Pineda Salgado <edinson110895@gmail.com>
2026-04-22 21:29:04 -0500

    fix: resolve black screen by removing crossOrigin from player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -273,7 +273,6 @@ export default function Player({ channel, onClose, playlist = [], onPlayNext, on
               controls 
               autoPlay 
               playsInline 
-              crossOrigin="anonymous"
             />
           )}
 
